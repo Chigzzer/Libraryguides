6eb0264d35d0954338c993bdaac82df9dcc40e16
Chiraag Chandarana <chiraag.chandarana@gmail.com>
2018-08-10 22:36:03 +0100

    Added git guide

---
+++ git guide.py
@@ -0,0 +1,31 @@
+git init  # creates a git in the folder where you entered bash
+
+git touch xxxx.zz  # Creates a file in the folder with xxxx.zz being the file name and extension
+
+git add filename  # adds the file to be committed
+
+git add *.extension  # addds all filenames to be committed
+
+git add .  # adds all files in folder to be committed.
+
+git commit -m 'Commit Message'  # commits the files to the branch with the message in between ""
+
+git push  # pushes the commited branch to github online
+
+git status  # List the files you've changed and those you still need to add or commit:
+
+git remote add origin <server>  # If you haven't connected your local repository to a remote server, add the server to be able to push to it
+
+git branch  # list all the branches
+
+git checkout -b <branchname>  # create a new branch and change to it
+
+git checkout <branchname>  # changes to new branch
+
+git branch -d <branchname>  # delete featured branch
+
+git pull  # Fetch and merge changes on the remote server to your working directory:
+
+git merge <branchname>  # To merge a different branch into your active branch:
+
+
